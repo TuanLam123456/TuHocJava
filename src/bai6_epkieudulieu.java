8ac49b6dc10d470b53840dc5f9c436a0d6840376
TuanLam123456 <130367417+TuanLam123456@users.noreply.github.com>
2025-09-10 11:13:23 +0700

    New files

---
+++ src/bai6_epkieudulieu.java
@@ -0,0 +1,18 @@
+public class bai6_epkieudulieu {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        double kq = (double) a/b;
+
+        //ép kiểu hẹp
+        int c = 128;
+        byte d = (byte) c;
+        System.out.println(c);
+        System.out.println(d);
+
+        int e = 15;
+        byte f = (byte) e;
+        System.out.println(e);
+        System.out.println(f);
+    }
+}
---
+++ src/bai7_cacpheptoancoban.java
@@ -0,0 +1,23 @@
+public class bai7_cacpheptoancoban {
+
+
+    public static void main(String[] args) {
+        int a = 11;
+        int b = 5;
+        System.out.println("a = " +a);
+        System.out.println("b = " +b);
+
+        //phép cộng
+        System.out.println("a + b = " + (a+b));
+
+
+        //Gắn biến
+        int kq = a + b;
+        System.out.println("a + b = " + kq);
+
+        System.out.println("a - b = " + (a-b));
+        System.out.println("a * b = " + (a*b));
+        System.out.println("a % b = " + (a%b));
+        System.out.println("a / b = " + ((double)(a/b)));
+    }
+}
